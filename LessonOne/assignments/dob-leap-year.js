2de29b6636e1bfd11dcf607fd0ad7ee0b6eb82aa
Neeraj Singh <869neerajsingh@gmail.com>
2023-10-26 14:34:51 +0530

    Write CLI program to check leap year DOB

---
+++ LessonOne/assignments/dob-leap-year.js
@@ -0,0 +1,27 @@
+import readLineSync from 'readline-sync';
+import chalk from 'chalk';
+
+function welcomeUser() {
+    let userName = readLineSync.question(chalk.inverse('Please Type your name:') + ' ');
+    console.log();
+    console.log(chalk.bold.underline.bgBlue("Hi " + userName + ", Welcome to Program to check Leap year DOB!"));
+}
+
+function startCheck() {
+    console.log();
+    
+    let userDOB = readLineSync.question(chalk.magenta("Enter your Date of Birth (as dd/mm/yyyy) - "));
+
+    console.log();
+
+    if ( userDOB.split('/').length !== 3 || isNaN(Number(userDOB.split('/')[2])) ) {
+        console.log(chalk.bold.redBright.inverse('You have entered invalid date of birth.'));
+    } else if ( Number(userDOB.split('/')[2]) % 4 === 0 ) {
+        console.log(chalk.bold.greenBright.inverse('Yay! Your date of birth comes under Leap year. Share this on social media.'));
+    } else {
+        console.log(chalk.bold.yellow.inverse('Sadly, Your date of birth comes under non-leap year.'));
+    }
+}
+
+welcomeUser();
+startCheck();
